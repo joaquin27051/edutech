e7c11c72f5d0387bdd5654011e278d55c8bdd74e
joaquin <joaq.pastenes@duocuc.cl>
2025-05-22 19:00:51 -0400

    Initial commit

---
+++ src/main/java/Edutech/edutech/EdutechApplication.java
@@ -0,0 +1,12 @@
+package Edutech.edutech;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechApplication.class, args);
+	}
+}
---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,11 @@
+package services;
+import models.User;
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    User createUser(User user);
+    User updateUser(Long id, User userDetails);
+    void deleteUser(Long id);
+}
